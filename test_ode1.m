2e2f2846cb38b3de38482bff511ac6e6cb546c03
thealexinator2904 <59167072+thealexinator2904@users.noreply.github.com>
2020-10-13 13:52:04 +0200

    Update test_ode1.m

--- test_ode1.m
+++ test_ode1.m
@@ -4,7 +4,7 @@ close all;
 
 
 t0 = 0;
-t_end = 10;
+t_end = 3;
 h = 0.001;
 
 y=1;
@@ -13,12 +13,11 @@ y=zeros(t_end/h+1, 1);
 y(1)=1;
 
 for i =2:t_end/h
-    y(i) = y(i-1)+h*eq1(y(i-1),i/h);
+    y(i) = y(i-1)+h*eq2(y(i-1),i*h);
 end
 plot(t,y)
 hold('on')
-y_ideal = exp(t./-0.5);
-plot(t,y_ideal);
+plot(t,eq2_ideal(t));
 grid('on')
 legend('ode', 'ideal solution')
 
@@ -26,6 +25,15 @@ function [dy]=eq1(y,t)
     tau = -0.5;
     dy = y/tau;
 end
-function [dy]=eq2(y,t)
-    dy=t
+function [y]=eq1_ideal(t)
+    tau = -0.5;
+    y= 1.* exp(t./-0.5);
 end
+
+function [dy]=eq2(y,t)
+    dy=t;
+end
+function [y]=eq2_ideal(t)
+    y= 1+ t.*t./2;
+end
+
