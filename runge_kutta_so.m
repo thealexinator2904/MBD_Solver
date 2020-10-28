2c817191af46621e1f19abb3371493460d4fa63e
Alexander Grebien <59167072+thealexinator2904@users.noreply.github.com>
2020-10-28 11:31:48 +0100

    continued work on runge kutta

--- runge_kutta_so.m
+++ runge_kutta_so.m
@@ -4,7 +4,9 @@ function [y]=runge_kutta_so(eq, t0, h, t_end)
     y(1)=1;
     
     for i =2:t_end/h
-      y(i) = y(i-1)+h*eq(y(i-1),i*h+t0);
+        k1=eq(y(i-1),i*h+t0);
+        k2=eq()
+        %      y(i) = y(i-1)+h*eq(y(i-1),i*h+t0);
     end
 end
 
